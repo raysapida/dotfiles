e9852ed6b05adfd07216a2ce49b18dce137c4fa6
raysapida <ray.sapida@gmail.com>
2019-04-18 21:27:35 -0700

    Install oh-my-zsh and antigen

--- setup/manjoro.sh
+++ setup/manjoro.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 pacman -Syy vim neovim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl -L git.io/antigen > ~raysapida/antigen.zsh
