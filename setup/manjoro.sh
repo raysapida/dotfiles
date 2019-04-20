610d8864e97da6bacbd61307068b52701413ae98
raysapida <ray.sapida@gmail.com>
2019-04-19 23:33:18 -0700

    Add notes for commands used to install yaourt and rcm

--- setup/manjoro.sh
+++ setup/manjoro.sh
@@ -4,3 +4,13 @@ pacman -Syy vim neovim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 curl -L git.io/antigen > ~raysapida/antigen.zsh
+
+# TODO: Change this to how it would be scripted instead of manually writing the commands
+# Install yaourt: https://cloudcone.com/docs/article/install-packages-in-arch-linux-from-aur/
+# git clone https://github.com/archlinuxfr/yaourt
+# cd yaourt/
+# makepkg -si
+
+# git clone https://aur.archlinux.org/rcm.git
+# cd rcm/
+# makepkg -si
