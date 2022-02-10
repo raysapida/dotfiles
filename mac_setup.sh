c56300f6f0b6a09e970fd61a44eafa30f3bbe6ff
Raymond Sapida <raymond@Raymonds-MacBook-Pro.local>
2022-02-10 07:41:12 -0800

    Start recording commands needed to install apps needed for mac
    - Not sure if mac_setup.sh can just run on its own without changes

---
+++ mac_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/raymond/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install node
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+brew update
+brew install neovim
+brew install tmux
+brew install rbenv
+brew install python
+
+brew install rcm
+lsrc
+rcup -v
+
+# Install antigen
+curl -L git.io/antigen > antigen.zsh
+
+# Install tpm (tmux plugin manager)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Dependencies for tmux-plugins/tmux-urlview
+brew install urlview
+brew install extract_url
+
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
